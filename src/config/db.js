de0388f7396b7565b48904423d10e3397927a43f
Rida Zaimus <ridazaimus15@gmail.com>
2023-12-09 07:57:20 +0800

    fix: timezone db after deploy

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,6 @@ const { Sequelize } = require('sequelize')
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
-  timezone: '+07:00', // Sesuaikan dengan zona waktu yang diinginkan (Asia/Jakarta)
-  dialectOptions: {
-    timezone: '+07:00', // Jika diperlukan, sesuaikan dengan zona waktu yang diinginkan
-  },
   port: process.env.DB_PORT
 });
 
