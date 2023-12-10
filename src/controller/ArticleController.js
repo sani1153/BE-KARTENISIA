ddef9df60ce5b18a30c540dc51fc17fa4676c0f4
Rida Zaimus <ridazaimus15@gmail.com>
2023-12-11 05:56:36 +0800

    feat: get random article

--- src/controller/ArticleController.js
+++ src/controller/ArticleController.js
@@ -82,36 +82,36 @@ const getArticlesByTittle = async (req, res) => {
     }
 };
 
-const getArticlesById  = async (req, res) => {
-    console.log(req)
-    try {
-        let articlesData;
-        // Mengambil data artikel
-        if(req.query.article_id){
-            articlesData = await articles.findAll({
-                where: {
-                    article_id: req.query.article_id
-                }
+// const getArticlesById  = async (req, res) => {
+//     console.log(req)
+//     try {
+//         let articlesData;
+//         // Mengambil data artikel
+//         if(req.query.article_id){
+//             articlesData = await articles.findAll({
+//                 where: {
+//                     article_id: req.query.article_id
+//                 }
         
-            });
-        }else{
-            articlesData = await articles.findAll()
-        }
+//             });
+//         }else{
+//             articlesData = await articles.findAll()
+//         }
 
-        res.json({
-            message: 'Get Article Success',
-            data: articlesData // Mengirim data artikel sebagai respons
-        });
+//         res.json({
+//             message: 'Get Article Success',
+//             data: articlesData // Mengirim data artikel sebagai respons
+//         });
 
-    } catch (error) {
-        res.status(500).json({
-            message: "Server Error",
-            serverMessage : error
-        })
-    }
-}
+//     } catch (error) {
+//         res.status(500).json({
+//             message: "Server Error",
+//             serverMessage : error
+//         })
+//     }
+// }
 
-const getArticlesById2 = async (req, res) => {
+const getArticlesById = async (req, res) => {
     try {
       const { id } = req.params;
       const article = await articles.findByPk(id);
@@ -126,7 +126,7 @@ const getArticlesById2 = async (req, res) => {
     }
   }
 
-  const getArticlesByDate = async (req, res) => {
+const getArticlesByDate = async (req, res) => {
     try {
       const { order } = req.query; // 'asc' or 'desc'
       
@@ -179,6 +179,35 @@ const getArticlesById2 = async (req, res) => {
       res.status(500).json({ message: 'Terjadi kesalahan dalam mengambil Artikel berdasarkan tanggal dan kategori' });
     }
   };
+
+  const getRandomArticle = async () => {
+    try {
+      // Mendapatkan jumlah total artikel dalam basis data Anda
+      const totalArticles = await articles.count(); // Ganti Article dengan model Anda
+      
+      // Menghasilkan ID acak antara 1 dan jumlah total artikel
+      const randomId = Math.floor(Math.random() * totalArticles) + 1;
+      
+      // Mengambil artikel dengan ID acak yang dihasilkan
+      const article = await articles.findByPk(randomId); // Ganti Article dengan model Anda
+
+      if (!article) {
+        return { error: 'Artikel tidak ditemukan' };
+      }
+      return article;
+
+    } catch (error) {
+      console.error('Terjadi kesalahan:', error);
+      return { error: 'Terjadi kesalahan dalam mengambil artikel' };
+    }
+  };
+  
+  // Penggunaan fungsi getRandomArticle
+  getRandomArticle().then(article => {
+    console.log(article);
+    // Lakukan sesuatu dengan artikel yang didapatkan
+  });
+  
 // function backfillArticle(req, res, next){
 //     articles.bulkCreate([{
 //         title: "Article 1",
@@ -210,7 +239,7 @@ module.exports = {
     getArticles,
     getArticlesByTittle,
     getArticlesById,
-    getArticlesById2,
     getArticlesByDate,
-    getArticlesByDateAndCategory
+    getArticlesByDateAndCategory,
+    getRandomArticle
 }
--- src/routes/ArticleRoutes.js
+++ src/routes/ArticleRoutes.js
@@ -6,9 +6,9 @@ const router = express.Router()
 router.get('/articles', ArticleRoute.getArticles);
 router.post('/post-article', ArticleRoute.addArticle);
 router.get('/articles/search', ArticleRoute.getArticlesByTittle)
-router.get('/article', ArticleRoute.getArticlesById)
-router.get('/news/:id', ArticleRoute.getArticlesById2)
+router.get('/news/:id', ArticleRoute.getArticlesById)
 router.get('/new', ArticleRoute.getArticlesByDate)
 router.get('/berita', ArticleRoute.getArticlesByDateAndCategory)
+router.get('/random', ArticleRoute.getRandomArticle)
 
 module.exports = router;
