2c8f28f6e5573ea760f999ca693d8d6fd0a67797
Rida Zaimus <ridazaimus15@gmail.com>
2023-12-11 16:55:46 +0800

    fix

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const createNewUser = require('../controller/UserController.js')
 const verify = require('../middleware/LogoutVerify.js');
-const verifyToken = require('../middleware/LoginVerify.js');
+// const verifyToken = require('../middleware/LoginVerify.js');
 
 const router = express.Router();
 
 router.post("/signup", createNewUser.createNewUser)
-router.post("/login", createNewUser.createNewUser)
-router.post("api/user", verifyToken, (req, res) => {
-    res.json({ userData: req.userData });
-})
-
+router.post("/login", createNewUser.loginController)
 router.get("/users", createNewUser.getUsers)
 router.post("/logout", verify, createNewUser.logoutController)
 router.get('/user/:id', createNewUser.getUserById)
-
-
 
 module.exports = router;
